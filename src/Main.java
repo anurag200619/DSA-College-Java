4a608926a1ff714d377618c25f0c62918fb666aa
anurag200619 <anurag190605@gmail.com>
2026-04-15 12:27:25 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -281,6 +281,33 @@ import java.util.*;
 //    }
 //}
 
-public class Main{
-    
-}
+//public class Main{
+//    static int fib(int n)
+//    {
+//        if(n<=1) return n;
+//        return fib(n-1)+fib(n-2);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println(fib(5));
+//    }
+//}
+
+//public class Main{
+//    static void subset(String p, String up){
+//        // base case
+//        if(up.isEmpty()){
+//            System.out.println(p);
+//            return;
+//        }
+//        char ch = up.charAt(0);
+//
+//        // include
+//        subset(p + ch, up.substring(1));
+//        // exclude
+//        subset(p, up.substring(1));
+//    }
+//    public static void main(String[] args) {
+//        String str = "abc";
+//        subset("", str);
+//    }
+//}
