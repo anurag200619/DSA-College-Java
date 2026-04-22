67937e0edd7877f162281f0e16ae68920424cbbf
anurag200619 <anurag190605@gmail.com>
2026-04-22 10:28:19 +0530

    init commit

--- src/Main.java
+++ src/Main.java
@@ -315,6 +315,22 @@ import java.util.*;
 public class Main{
     static void subset(String p, String up){
 
-        
-    }
-}
+//public class Main {
+//
+//    // Recursive function to find sum
+//    public static int sumArray(int[] arr, int n) {
+//        // Base case
+//        if (n <= 0) {
+//            return 0;
+//        }
+//        // Recursive case
+//        return arr[n - 1] + sumArray(arr, n - 1);
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {1, 2, 3, 4, 5};
+//
+//        int result = sumArray(arr, arr.length);
+//        System.out.println("Sum of array = " + result);
+//    }
+//}
